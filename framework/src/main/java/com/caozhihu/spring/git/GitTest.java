ae9522b5e763e8e7b190de54e5c4cfb87dbacafe
Rorty <rortywang@vesync.com.cn>
2021-10-18 20:26:39 +0800

    [feat]版本2提交了

--- framework/src/main/java/com/caozhihu/spring/git/GitTest.java
+++ framework/src/main/java/com/caozhihu/spring/git/GitTest.java
@@ -2,13 +2,15 @@ package com.caozhihu.spring.git;
 
 /**
  * @author peng.wang
- * @Description TODO
+ * @Description 我是版本2提交的"
  * @createTime 2021/10/18
  */
 public class GitTest {
 
     public static void main(String[] args) {
+        System.out.println("我是版本2提交的");
         System.out.println("我是版本1提交的");
+        System.out.println("我是版本2提交的");
     }
 
 }
