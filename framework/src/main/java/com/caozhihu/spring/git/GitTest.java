95c7af7eb6a4c16a1a61de729058aea301de3871
Rorty <rortywang@vesync.com.cn>
2021-10-18 20:29:11 +0800

    [feat]版本3提交了

--- framework/src/main/java/com/caozhihu/spring/git/GitTest.java
+++ framework/src/main/java/com/caozhihu/spring/git/GitTest.java
@@ -8,6 +8,8 @@ package com.caozhihu.spring.git;
 public class GitTest {
 
     public static void main(String[] args) {
+        String temp_version = "3";
+        System.out.println("我是版本" + temp_version + "提交的");
         System.out.println("我是版本1提交的");
     }
 
